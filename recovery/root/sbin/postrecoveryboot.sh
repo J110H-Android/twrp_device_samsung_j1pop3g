47d621339667f26c177fc328731ba98a7af0d9bd
TriDiscord <67373913+TriDiscord@users.noreply.github.com>
2022-06-16 10:10:30 +0300

    recovery: root: sbin: Fix script
    Thanks @DarthJabba9 for the fix

--- recovery/root/sbin/postrecoveryboot.sh
+++ recovery/root/sbin/postrecoveryboot.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-mkdir -p /flashlight
-cd /flashlight
-ln -s /sys/devices/virtual/camera/rear/rear_flash brightness
-echo 1 > max_brightness
-
+mkdir /flashlight
+ln -s /sys/devices/virtual/camera/rear/rear_flash /flashlight/brightness
+echo 1 > /flashlight/max_brightness
